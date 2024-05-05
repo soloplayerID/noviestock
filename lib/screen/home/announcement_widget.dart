f69a449d4168c68846abbddb2a410284e4ede194
Ilham Taufik <ilhamtaufik@Ilhams-Mac-mini.local>
2024-05-05 11:12:53 +0700

    benerin announcement, sama bikin email

--- lib/screen/home/announcement_widget.dart
+++ lib/screen/home/announcement_widget.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:novistock/helper/constants.dart';
 
 class AnnouncementWidget extends StatefulWidget {
@@ -11,51 +12,91 @@ class AnnouncementWidget extends StatefulWidget {
 
 class _AnnouncementWidgetState extends State<AnnouncementWidget> {
   var db = FirebaseFirestore.instance;
+  String today = '';
+
+  @override
+  void initState() {
+    today = getFormattedDate();
+    super.initState();
+  }
+
+  String getFormattedDate() {
+    final currentDate = DateTime.now();
+    final formattedDate = DateFormat('yyyy-MM-dd').format(currentDate);
+    return formattedDate;
+  }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 88,
-      child: ListView.builder(
-        itemCount: 1,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: ((context, index) {
-          return InkWell(
-            onTap: () {},
-            child: Container(
-              padding: const EdgeInsets.all(9),
-              margin: const EdgeInsets.only(right: 20),
-              width: 208,
-              height: 100,
-              decoration: BoxDecoration(
-                  color: kWhite,
-                  borderRadius: BorderRadius.circular(kBorderRadius),
-                  boxShadow: [
-                    BoxShadow(
-                      color: kDarkBlue.withOpacity(0.051),
-                      offset: const Offset(0.0, 3.0),
-                      blurRadius: 24.0,
-                      spreadRadius: 0.0,
-                    ),
-                  ]),
-              child: Column(
-                children: [
-                  Text('Stock Expired',
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                      style:
-                          kPoppinsSemiBold.copyWith(fontSize: 14, color: kRed)),
-                  Text('Barang mendekati masa expired',
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                      style: kPoppinsRegularBold.copyWith(
-                        fontSize: 12,
-                      )),
-                ],
-              ),
-            ),
+      child: StreamBuilder<QuerySnapshot>(
+        stream: db
+            .collection('stocks')
+            .where('expired_date', isLessThanOrEqualTo: today)
+            .snapshots(),
+        builder: (context, snapshots) {
+          if (snapshots.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshots.hasError) {
+            return const Center(child: Text('Error'));
+          }
+
+          final dataProducts = snapshots.data!.docs;
+
+          if (dataProducts.isEmpty) {
+            return const Center(
+              child: Text('No expired stocks found'),
+            );
+          }
+
+          return ListView.builder(
+            scrollDirection: Axis.horizontal,
+            shrinkWrap: true,
+            itemCount: dataProducts.length,
+            itemBuilder: (context, index) {
+              final productData = dataProducts[index].data();
+              return InkWell(
+                onTap: () {},
+                child: Container(
+                  padding: const EdgeInsets.all(9),
+                  margin: const EdgeInsets.only(right: 20),
+                  width: 208,
+                  height: 100,
+                  decoration: BoxDecoration(
+                      color: kWhite,
+                      borderRadius: BorderRadius.circular(kBorderRadius),
+                      boxShadow: [
+                        BoxShadow(
+                          color: kDarkBlue.withOpacity(0.051),
+                          offset: const Offset(0.0, 3.0),
+                          blurRadius: 24.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ]),
+                  child: Column(
+                    children: [
+                      Text('Stock Expired $today',
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                          style: kPoppinsSemiBold.copyWith(
+                              fontSize: 14, color: kRed)),
+                      Text('Barang mendekati masa expired',
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                          style: kPoppinsRegularBold.copyWith(
+                            fontSize: 12,
+                          )),
+                    ],
+                  ),
+                ),
+              );
+            },
           );
-        }),
+        },
       ),
     );
   }
